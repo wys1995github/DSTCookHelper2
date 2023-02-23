c3a2830ec7174330438f851dcdf707dcbc58c89b
wys1995github <458080078@qq.com>
2023-02-23 15:21:18 +0800

    基础提交

--- src/main/java/dstch/bean/CLBean.java
+++ src/main/java/dstch/bean/CLBean.java
@@ -1,65 +1,79 @@
 package main.java.dstch.bean;
 
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public class CLBean {
 
+	private final SimpleIntegerProperty num;
+	private final SimpleStringProperty name;
 	private final SimpleStringProperty item1;
 	private final SimpleStringProperty item2;
 	private final SimpleStringProperty item3;
 	private final SimpleStringProperty item4;
 	
-	CLBean(String item1, String item2, String item3, String item4){
+	public CLBean(int num, String name, String item1, String item2, String item3, String item4){
+		this.num = new SimpleIntegerProperty(num);
+		this.name = new SimpleStringProperty(name);
 		this.item1 = new SimpleStringProperty(item1);
 		this.item2 = new SimpleStringProperty(item2);
 		this.item3 = new SimpleStringProperty(item3);
 		this.item4 = new SimpleStringProperty(item4);
 	}
 	
+	public int getNum() {
+		return this.num.get();
+	}
+	public String getName() {
+		return this.name.get();
+	}
 	public String getItem1() {
 		return this.item1.get();
 	}
-	
 	public String getItem2() {
 		return this.item2.get();
 	}
-	
 	public String getItem3() {
 		return this.item3.get();
 	}
-	
 	public String getItem4() {
 		return this.item4.get();
 	}
 	
+	public void setNum(int num) {
+		this.num.set(num);
+	}
+	public void setName(String name) {
+		this.name.set(name);
+	}
 	public void setItem1(String item1) {
 		this.item1.set(item1);
 	}
-	
 	public void setItem2(String item2) {
 		this.item2.set(item2);
 	}
-	
 	public void setItem3(String item3) {
 		this.item3.set(item3);
 	}
-	
 	public void setItem4(String item4) {
 		this.item4.set(item4);
 	}
 	
+	public SimpleIntegerProperty getNumProperty() {
+		return num;
+	}
+	public SimpleStringProperty getNameProperty() {
+		return name;
+	}
 	public SimpleStringProperty getItem1Property() {
 		return item1;
 	}
-
 	public SimpleStringProperty getItem2Property() {
 		return item2;
 	}
-
 	public SimpleStringProperty getItem3Property() {
 		return item3;
 	}
-
 	public SimpleStringProperty getItem4Property() {
 		return item4;
 	}
--- src/main/java/dstch/service/InfoService.java
+++ src/main/java/dstch/service/InfoService.java
@@ -5,25 +5,35 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import main.java.dstch.bean.CLBean;
 import main.java.dstch.util.DBUtil;
 
 public class InfoService{
 
 	private Connection conn = DBUtil.getConnection();
 	private PreparedStatement ps = null;
+	private ResultSet rs = null;
 
-	public void getInfoData() {
+	public ObservableList<CLBean> getInfoData(String searchKey) {
 		String sql = " select cl_item1, cl_item2, cl_item3, cl_item4 "
-				+ "from t_compositeList where cl_name = '肉丸' ";
-		ResultSet rs = null;
+				+ "from t_compositeList where cl_name = ? ";
+		ObservableList<CLBean> clDataList = FXCollections.observableArrayList();
 		try {
 			ps = conn.prepareStatement(sql.toString());
+			ps.setString(1, "%" + searchKey + "%");
 			rs = ps.executeQuery();
+			int cl_num = 1;
 			while(rs.next()) {
-				System.out.println(rs.getString("cl_item1"));
-				System.out.println(rs.getString("cl_item2"));
-				System.out.println(rs.getString("cl_item3"));
-				System.out.println(rs.getString("cl_item4"));
+				clDataList.add(new CLBean(
+					cl_num++,
+					rs.getString("cl_name"),
+					rs.getString("cl_item1"),
+					rs.getString("cl_item2"),
+					rs.getString("cl_item3"),
+					rs.getString("cl_item4")
+				));
 			}
 		} catch (SQLException e){
 			e.printStackTrace();
@@ -32,10 +42,25 @@ public class InfoService{
             DBUtil.closePreparedStatement(ps);
             DBUtil.closeConnection(conn);
 		}
+		return clDataList;
 	}
 	
-	public static void main(String[] args) {
-		new InfoService().getInfoData();
-	}
+//	private Icon getIconAsItem(String itemName) {
+//		String sql = " select fi_image from t_foodIngredients where fi_name = ? ";
+//		ResultSet rs = null;
+//		Icon icon = null;
+//		try {
+//			ps = conn.prepareStatement(sql.toString());
+//			ps.setString(1, itemName);
+//			rs = ps.executeQuery();
+//			while(rs.next()) {
+//				icon = ImageUtil.loadImage(rs.getBinaryStream("fi_image"));
+//			}
+//			return icon;
+//		} catch (SQLException e){
+//			e.printStackTrace();
+//		}
+//		return icon;
+//	}
 	
 }
