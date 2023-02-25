0c3f419d83e1388b81528ac18de350618e9a3057
wys1995github <458080078@qq.com>
2023-02-25 22:40:49 +0800

    文件名称修改

--- src/main/java/dstch/view/InfoView.java
+++ src/main/java/dstch/view/InfoView.java
@@ -1,16 +1,16 @@
 package main.java.dstch.view;
 
-import java.io.IOException;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class InfoView {
 
 	public static void load(Stage stage) {
 		try {
-			Parent root = FXMLLoader.load(InfoView.class.getResource("/fxml/cookedDishesView.fxml"));
+			Parent root = FXMLLoader.load(InfoView.class.getResource("/fxml/CookedDishesView.fxml"));
 			stage.getScene().setRoot(root);
 		} catch (IOException e) {
 			e.printStackTrace();
